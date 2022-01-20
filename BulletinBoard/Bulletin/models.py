2700d79b75418253a2bcef15c849fabc1eb1894e
KrasovskihIA <75630213+KrasovskihIA@users.noreply.github.com>
2022-01-20 22:43:04 +0400

    Регистрация Моделей
    Регистрация Моделей

--- BulletinBoard/Bulletin/models.py
+++ BulletinBoard/Bulletin/models.py
@@ -2,19 +2,25 @@ from tabnanny import verbose
 from unicodedata import category
 from django.db import models
 from django.contrib.auth.models import User
-from django.db.models.deletion import CASCADE
+from django.db.models.deletion import CASCADE, SET_NULL
 from django.utils import timezone
 from django.db.models.fields import DateTimeField
 
+
+
 # Класс профиля
 class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=CASCADE, related_name='user_profile')
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='user_profile')
     birth_data = models.DateField('Дата регистрации', blank=False, auto_now_add=True, null=False)
     about = models.TextField(max_length=1000, blank=True)
     avatar = models.ImageField(upload_to='avatars/')
 
     def __str__(self):
         return f'{self.user.username}'
+
+    class Meta:
+        verbose_name = 'Профиль'
+        verbose_name_plural = 'Профили'
 
 # Класс категорий
 class Category(models.Model):
@@ -29,6 +35,7 @@ class Category(models.Model):
         verbose_name = 'Категория'
         verbose_name_plural = 'Категории'
 
+
 # Класс обьявлений
 class Post(models.Model):
     author = models.ForeignKey(User, on_delete=CASCADE)
@@ -37,8 +44,18 @@ class Post(models.Model):
     image = models.ImageField(upload_to='posts/')
     date_pub = models.DateTimeField(default=timezone.now)
     date_edit = models.DateTimeField(default=timezone.now)
-    category = models.ForeignKey(Category, blank=False, on_delete=CASCADE)
-    likes = models.ManyToManyField(User, on_delete=CASCADE, blank=True, related_name='users_likes_it')
+    category = models.ForeignKey(Category, verbose_name='Категория', blank=False, on_delete=models.SET_NULL, null=True)
+    likes = models.ManyToManyField(User, blank=True, related_name='users_likes_it')
+    contact = models.CharField(max_length=15)
+    url = models.SlugField(max_length=150, unique=True)
+
+    def __str__(self):
+        return f'{self.title}'
+
+    class Meta:
+        verbose_name = 'Объявление'
+        verbose_name_plural = 'Объявления'
+
 
 
 # Класс комментариев
@@ -47,4 +64,8 @@ class Comment(models.Model):
     text = models.TextField(max_length=500)
     in_post = models.ForeignKey(Post, on_delete=CASCADE)
     date_publish = models.DateTimeField(default=timezone.now)
+
+    class Meta:
+        verbose_name = 'Комментарий'
+        verbose_name_plural = 'Комментарий'
 
