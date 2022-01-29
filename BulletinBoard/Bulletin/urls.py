c371c8244c3a3e037f03d98ffc3fa22b3a3f6b35
KrasovskihIA <75630213+KrasovskihIA@users.noreply.github.com>
2022-01-29 23:24:50 +0400

    Детальное представление объявления

--- BulletinBoard/Bulletin/urls.py
+++ BulletinBoard/Bulletin/urls.py
@@ -1,6 +1,13 @@
 from django.urls import path
 from . import views
 
+
+
 urlpatterns = [
     path('', views.IndexViews.as_view(), name='index'),
+    path('create/', views.post_create, name='post_create'),
+    path('post/<int:post_id>/', views.post_detail, name='post_detail'),
+    path('post/<int:post_id>/edit/', views.post_edit, name='post_edit'),
+    path('post/<int:post_id>/delete/', views.post_delete, name='post_delete'),
+    path('post/<int:post_id>/like/', views.like_post, name='like_post'),
 ]
--- BulletinBoard/Bulletin/views.py
+++ BulletinBoard/Bulletin/views.py
@@ -1,5 +1,5 @@
 from msilib.schema import ListView
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views.generic.base import View
 from django.http import HttpResponse
 from .models import *
@@ -8,4 +8,24 @@ from .models import *
 class IndexViews(View):
     def get(self, request):
         posts = Post.objects.all()
-        return render(request,'posts/index.html', {'post_list':posts})
+        return render(request,'BulletinBoard/index.html', {'post_list':posts})
+
+
+def post_detail(request, post_id):
+    post = get_object_or_404(Post, id=post_id)
+    return render(request,'BulletinBoard/post_detail.html', {'post':post})
+
+
+
+
+def post_edit(request, post_id):
+    return HttpResponse(f'Редактирование обьявления номер {post_id}')
+
+def post_create(request):
+    return HttpResponse(f'Создание нового обьявления')
+
+def post_delete(request, post_id):
+    return HttpResponse(f'Удаление обьявления номер {post_id}')
+
+def like_post(request, post_id):
+    return HttpResponse(f'Лайк обьявления {post_id}')
