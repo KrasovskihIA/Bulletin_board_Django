15263ac9663e8a5aa0a31517bbce13470f0dd45e
KrasovskihIA <75630213+KrasovskihIA@users.noreply.github.com>
2022-01-30 17:41:08 +0400

    Лайк Объявления
    Представление для лайка под объявлением

--- BulletinBoard/Bulletin/views.py
+++ BulletinBoard/Bulletin/views.py
@@ -1,5 +1,5 @@
 from msilib.schema import ListView
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.views.generic.base import View
 from django.http import HttpResponse
 from .models import *
@@ -28,4 +28,10 @@ def post_delete(request, post_id):
     return HttpResponse(f'Удаление обьявления номер {post_id}')
 
 def like_post(request, post_id):
-    return HttpResponse(f'Лайк обьявления {post_id}')
+    post = get_object_or_404(Post, id=post_id)
+    if request.user in post.likes.all():
+        post.likes.remove(request.user)
+    else:
+        post.likes.add(request.user)
+        post.save()
+    return redirect(request.META.get('HTTP_REFERER'), request)
