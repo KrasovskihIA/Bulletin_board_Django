4eecb98f8affbb518e4acd8154dcb08324148858
KrasovskihIA <75630213+KrasovskihIA@users.noreply.github.com>
2022-02-01 22:33:41 +0400

    Class-based views
    - Создание объявления
    -Редактирование объявления
    -Удаление объявления
    -Лайк под объявлением

--- BulletinBoard/Bulletin/urls.py
+++ BulletinBoard/Bulletin/urls.py
@@ -1,13 +1,15 @@
 from django.urls import path
+from django.views.generic import TemplateView
 from . import views
 
 
 
 urlpatterns = [
-    path('', views.IndexViews.as_view(), name='index'),
-    path('create/', views.post_create, name='post_create'),
+    path('', views.IndexView.as_view(), name='index'),
+    path('create/', views.PostCreateView.as_view(), name='post_create'),
     path('post/<int:post_id>/', views.post_detail, name='post_detail'),
-    path('post/<int:post_id>/edit/', views.post_edit, name='post_edit'),
-    path('post/<int:post_id>/delete/', views.post_delete, name='post_delete'),
-    path('post/<int:post_id>/like/', views.like_post, name='like_post'),
+    path('post/<int:post_id>/edit/', views.PostEditView.as_view(), name='post_edit'),
+    path('post/<int:post_id>/delete/', views.PostDeleteView.as_view(), name='post_delete'),
+     path('post/<int:post_id>/delete_success/', TemplateView.as_view(template_name='BulletinBoard/delete_success.html'), name ='post_delete_success'),
+    path('post/<int:post_id>/like/', views.PostLikeView.as_view(), name='like_post'),
 ]
--- BulletinBoard/Bulletin/views.py
+++ BulletinBoard/Bulletin/views.py
@@ -1,15 +1,35 @@
+from django.core.exceptions import PermissionDenied
+from ast import Delete
+from audioop import reverse
 from msilib.schema import ListView
 from django.shortcuts import render, get_object_or_404, redirect
+from django.urls import reverse
 from django.views.generic.base import View
 from django.http import HttpResponse
+from django.views.generic import ListView, CreateView, DeleteView, UpdateView
 from .models import *
 from .forms import PostForm
+from django.utils.decorators import method_decorator
+from django.contrib.auth.decorators import login_required
 
 
-class IndexViews(View):
-    def get(self, request):
-        posts = Post.objects.all()
-        return render(request,'BulletinBoard/index.html', {'post_list':posts})
+class IndexView(ListView):
+    model = Post
+    template_name = 'BulletinBoard/index.html'
+    context_object_name = 'post_list'
+
+    def get_queryset(self):
+        return self.model.objects.filter(date_pub__lte=timezone.now()).order_by('-date_pub')[:16]
+
+
+class PostDeleteView(DeleteView):
+    model = Post
+    pk_url_kwarg = 'post_id'
+    template_name = 'BulletinBoard/post_delete.html'
+
+    def get_success_url(self):
+        post_id = self.kwargs['post_id']
+        return reverse('post_delete_success', args=(post_id, ))
 
 
 def post_detail(request, post_id):
@@ -18,40 +38,55 @@ def post_detail(request, post_id):
 
 
 
+class PostEditView(UpdateView):
+    model = Post
+    pk_url_kwarg = 'post_id'
+    template_name = 'BulletinBoard/post_edit.html'
+    form_class = PostForm
 
-def post_edit(request, post_id):
-    return HttpResponse(f'Редактирование обьявления номер {post_id}')
+    def dispatch(self, request, *args, **kwargs):
+        obj = self.get_object()
+        if obj.author != request.user:
+            raise PermissionDenied('Вы не автор этого объявления')
+        else:
+            return super(PostEditView, self).dispatch(request, *args, **kwargs)
+
+    def get_success_url(self):
+        post_id = self.kwargs[self.pk_url_kwarg]
+        return reverse('post_detail', args=(post_id, ))
 
 
-def post_create(request):
+
+class PostCreateView(CreateView):
+    form_class = PostForm
     template_name = 'BulletinBoard/post_create.html'
-    context = {'form': PostForm()}
 
-    if request.method == 'POST':
-        form = PostForm(request.POST, request.FILES)
+    @method_decorator(login_required)
+    def post(self, request, *args, **kwargs):
+        form = self.form_class(request.POST, request.FILES)
+        context = {}
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
             post.save()
-            context['post_was_created'] = True      
+            context['post_was_created'] = True  
+            context['form'] = self.form_class 
         else:
             context['post_with_errors'] = True
             context['form'] = form
-    return render(request, template_name, context)
+        return render(request, self.template_name, context)
 
+
+class PostLikeView(View):
+    def get(self, request, post_id, *args, **kwargs):
+        return redirect(reverse('post', args=(post_id, )))
+    
+    def post(self, request, post_id, *args, **kwargs):
+        post = get_object_or_404(Post, id=post_id)
+        if request.user in post.likes.all():
+            post.likes.remove(request.user)
+        else:
+            post.likes.add(request.user)
+            post.save()
+        return redirect(request.META.get('HTTP_REFERER'), request)
         
-
-
-
-
-def post_delete(request, post_id):
-    return HttpResponse(f'Удаление обьявления номер {post_id}')
-
-def like_post(request, post_id):
-    post = get_object_or_404(Post, id=post_id)
-    if request.user in post.likes.all():
-        post.likes.remove(request.user)
-    else:
-        post.likes.add(request.user)
-        post.save()
-    return redirect(request.META.get('HTTP_REFERER'), request)
