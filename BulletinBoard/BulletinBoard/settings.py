e4d166613c38eff3df0a9bffd4edfb8ba45d8145
KrasovskihIA <75630213+KrasovskihIA@users.noreply.github.com>
2022-02-05 17:43:31 +0400

    Подтверждение сброса пароля по почте
    Подтверждение сброса пароля по почте. Хост яндекс почта

--- BulletinBoard/BulletinBoard/settings.py
+++ BulletinBoard/BulletinBoard/settings.py
@@ -121,4 +121,10 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_HOST = 'smtp.yandex.ru'
+EMAIL_PORT = 465
+EMAIL_HOST_USER = ""
+EMAIL_HOST_PASSWORD = ""
+EMAIL_USE_TLS = False
+EMAIL_USE_SSL = True
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
